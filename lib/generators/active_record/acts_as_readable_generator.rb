312f365c2d055c5199753260d44d5125cf8bf195
Gen Takahashi <gendosu@gmail.com>
2017-07-19 11:28:36 +0900

    fix indent

--- lib/generators/active_record/acts_as_readable_generator.rb
+++ lib/generators/active_record/acts_as_readable_generator.rb
@@ -2,30 +2,30 @@ require 'rails/generators/active_record'
 
 module ActiveRecord
   module Generators
-class ActsAsReadableGenerator < ActiveRecord::Generators::Base
+    class ActsAsReadableGenerator < ActiveRecord::Generators::Base
 
-  source_root File.expand_path("../templates", __FILE__)
+      source_root File.expand_path("../templates", __FILE__)
 
-  def copy_initializer
-  end
+      def copy_initializer
+      end
 
-  def manifest
-      migration_template 'migration.rb', "db/migrate/#{file_name}", migration_version: migration_version
-  end
+      def manifest
+          migration_template 'migration.rb', "db/migrate/#{file_name}", migration_version: migration_version
+      end
 
-  def file_name
-    "acts_as_readable_migration"
-  end
+      def file_name
+        "acts_as_readable_migration"
+      end
 
-  def rails5?
-    Rails.version.start_with? '5'
-  end
+      def rails5?
+        Rails.version.start_with? '5'
+      end
 
-  def migration_version
-    if rails5?
-      "[#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}]"
+      def migration_version
+        if rails5?
+          "[#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}]"
+        end
+      end
     end
   end
-end
-end
 end
